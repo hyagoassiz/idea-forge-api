88adc457189c4038b889dbdbb3c198e58150e713
hyagoassiz <hyagoassiz@hotmail.com>
2026-08-20 06:20:25 -0300

    first commit

---
+++ src/main/java/com/idea_forge/IdeaForgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.idea_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaForgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaForgeApiApplication.class, args);
+	}
+
+}
