3663aac62c5c3aba5f3f42b0f81dd6c80d93ae1e
hyagoassiz <hyagoassiz@hotmail.com>
2026-08-26 18:48:47 -0300

    removendo endpoints

--- src/main/java/com/idea_forge/modules/user/controller/UserController.java
+++ src/main/java/com/idea_forge/modules/user/controller/UserController.java
@@ -1,17 +1,10 @@
 package com.idea_forge.modules.user.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.idea_forge.modules.user.dto.LoginRequestDTO;
-import com.idea_forge.modules.user.dto.LoginResponseDTO;
-import com.idea_forge.modules.user.dto.TokenResponseDTO;
 import com.idea_forge.modules.user.dto.UserRequestDTO;
 import com.idea_forge.modules.user.dto.UserResponseDTO;
 import com.idea_forge.modules.user.service.JwtService;
@@ -24,11 +17,11 @@ import jakarta.validation.Valid;
 public class UserController {
 
         private final UserService userService;
-        private final JwtService jwtService;
+        // private final JwtService jwtService;
 
         public UserController(UserService userService, JwtService jwtService) {
                 this.userService = userService;
-                this.jwtService = jwtService;
+                // this.jwtService = jwtService;
         }
 
         @PostMapping
@@ -37,52 +30,52 @@ public class UserController {
                 return userService.createUser(userRequestDTO);
         }
 
-        @PostMapping("/login")
-        public ResponseEntity<LoginResponseDTO> login(
-                        @RequestBody LoginRequestDTO loginRequestDTO) {
-                TokenResponseDTO tokens = userService.login(loginRequestDTO);
+        // @PostMapping("/login")
+        // public ResponseEntity<LoginResponseDTO> login(
+        // @RequestBody LoginRequestDTO loginRequestDTO) {
+        // TokenResponseDTO tokens = userService.login(loginRequestDTO);
 
-                LoginResponseDTO response = new LoginResponseDTO(
-                                "Login realizado com sucesso",
-                                loginRequestDTO.getEmail());
+        // LoginResponseDTO response = new LoginResponseDTO(
+        // "Login realizado com sucesso",
+        // loginRequestDTO.getEmail());
 
-                return ResponseEntity.ok()
-                                .header(
-                                                HttpHeaders.SET_COOKIE,
-                                                jwtService.generateAccessTokenCookie(tokens.getAccessToken()))
-                                .header(
-                                                HttpHeaders.SET_COOKIE,
-                                                jwtService.generateRefreshTokenCookie(tokens.getRefreshToken()))
-                                .body(response);
-        }
+        // return ResponseEntity.ok()
+        // .header(
+        // HttpHeaders.SET_COOKIE,
+        // jwtService.generateAccessTokenCookie(tokens.getAccessToken()))
+        // .header(
+        // HttpHeaders.SET_COOKIE,
+        // jwtService.generateRefreshTokenCookie(tokens.getRefreshToken()))
+        // .body(response);
+        // }
 
-        @GetMapping("/me")
-        public ResponseEntity<UserResponseDTO> getAuthenticatedUser() {
-                UserResponseDTO response = userService.getAuthenticatedUser();
-                return ResponseEntity.ok(response);
-        }
+        // @GetMapping("/me")
+        // public ResponseEntity<UserResponseDTO> getAuthenticatedUser() {
+        // UserResponseDTO response = userService.getAuthenticatedUser();
+        // return ResponseEntity.ok(response);
+        // }
 
-        @PostMapping("/refresh")
-        public ResponseEntity<LoginResponseDTO> refresh(
-                        @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+        // @PostMapping("/refresh")
+        // public ResponseEntity<LoginResponseDTO> refresh(
+        // @CookieValue(name = "refreshToken", required = false) String refreshToken) {
 
-                if (refreshToken == null || refreshToken.isEmpty()) {
-                        throw new RuntimeException("Refresh token não encontrado");
-                }
+        // if (refreshToken == null || refreshToken.isEmpty()) {
+        // throw new RuntimeException("Refresh token não encontrado");
+        // }
 
-                TokenResponseDTO tokens = userService.refresh(refreshToken);
+        // TokenResponseDTO tokens = userService.refresh(refreshToken);
 
-                LoginResponseDTO response = new LoginResponseDTO(
-                                "Token renovado com sucesso",
-                                null);
+        // LoginResponseDTO response = new LoginResponseDTO(
+        // "Token renovado com sucesso",
+        // null);
 
-                return ResponseEntity.ok()
-                                .header(
-                                                HttpHeaders.SET_COOKIE,
-                                                jwtService.generateAccessTokenCookie(tokens.getAccessToken()))
-                                .header(
-                                                HttpHeaders.SET_COOKIE,
-                                                jwtService.generateRefreshTokenCookie(tokens.getRefreshToken()))
-                                .body(response);
-        }
+        // return ResponseEntity.ok()
+        // .header(
+        // HttpHeaders.SET_COOKIE,
+        // jwtService.generateAccessTokenCookie(tokens.getAccessToken()))
+        // .header(
+        // HttpHeaders.SET_COOKIE,
+        // jwtService.generateRefreshTokenCookie(tokens.getRefreshToken()))
+        // .body(response);
+        // }
 }
