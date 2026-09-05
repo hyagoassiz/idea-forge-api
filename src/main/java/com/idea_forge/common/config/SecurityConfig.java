cb1be1ea45f2b5d3c017f5d922bf9ec065806e83
hyagoassiz <hyagoassiz@hotmail.com>
2026-09-05 19:04:19 -0300

    ID-11 fix(User): ajuste para reenviar token

--- src/main/java/com/idea_forge/common/config/SecurityConfig.java
+++ src/main/java/com/idea_forge/common/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.DispatcherType;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,6 +36,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users", "/users/login", "/users/refresh",
                                 "/auth/verify-email", "/auth/resend-verification-email", "/users/logout")
